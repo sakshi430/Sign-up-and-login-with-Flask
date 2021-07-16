b91b4dd296bb8665369e7c44e81030ac3052a50a
Sakshi Ingale <sakshi.ingale430@gmail.com>
2021-07-16 16:41:58 +0530

    sign up done

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
+from os import path
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
+DB_NAME="database.db"
 
 def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']="salskjds fudereoi"
+    app.config["SQLALCHEMY_DATABASE_URI"]=f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -10,4 +17,12 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')    
 
+    from .models import User,Note
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/'+DB_NAME):
+        db.create_all(app=app)
+        print('created database')
--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request
-from flask.helpers import flash
+from . import db
+from flask import Blueprint, render_template, request,flash, redirect,url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth=Blueprint('auth',__name__)
 
@@ -28,6 +30,10 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            new_user=User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()          
             flash('Account created.', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("signUp.html")        
